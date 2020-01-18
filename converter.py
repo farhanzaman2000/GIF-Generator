f284bc34cfe22760bbac55809b769fc91dc65e7b
farhanzaman2000 <55164384+farhanzaman2000@users.noreply.github.com>
2020-01-18 12:27:15 -0500

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,26 @@
+import imageio
+import os
+
+clip = os.path.abspath('video1.mp4')
+
+print(clip)
+
+def gifmaker(inputPath, targetFormat):
+    outputPath = os.path.splitext(inputPath)[0] + targetFormat
+    print(f'converting {inputPath} \n to {outputPath}')
+    reader = imageio.get_reader(inputPath)
+    fps = reader.get_meta_data()['fps']
+    writer = imageio.get_writer(outputPath, fps=fps)
+    for frames in reader:
+      writer.append_data(frames)
+      print(f'Frame{frames}')
+    
+    print('DONE')
+    writer.close()
+
+
+
+
+gifmaker(clip, '.gif')
+
+
